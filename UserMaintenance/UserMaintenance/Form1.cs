80d97aaf403b7e327fccbbadeee4a5e15e1a081d
corvinus-axx78y <levente.virag@stud.uni-corvinus.hu>
2021-10-01 11:33:09 +0200

    9-es pont

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using UserMaintenance.Entities;
+using System.IO;
 
 namespace UserMaintenance
 {
@@ -19,6 +20,7 @@ namespace UserMaintenance
             InitializeComponent();
             label1.Text = Resource1.FullName;
             button1.Text = Resource1.Add;
+            button2.Text = Resource1.Write;
 
             listBox1.DataSource = users;
             listBox1.ValueMember = "ID";
@@ -32,6 +34,22 @@ namespace UserMaintenance
                 FullName = textBox1.Text,
             };
             users.Add(u);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sf = new SaveFileDialog();
+            string filter = ("CSV file (*.csv)|*.csv");
+            sf.Filter = filter;
+            if(sf.ShowDialog() == DialogResult.OK)
+            {
+                StreamWriter sw = new StreamWriter(sf.FileName);
+                foreach (User item in listBox1.Items)
+                {
+                    sw.WriteLine(item.ID.ToString() + ";" + item.FullName.ToString());
+                }
+                sw.Close();
+            }
         }
     }
 }
