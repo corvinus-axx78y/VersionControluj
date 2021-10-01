3a96add1b45f3ff0112b9c5979a3e42637118cf1
corvinus-axx78y <levente.virag@stud.uni-corvinus.hu>
2021-10-01 11:44:04 +0200

    11-es pont

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -21,6 +21,7 @@ namespace UserMaintenance
             label1.Text = Resource1.FullName;
             button1.Text = Resource1.Add;
             button2.Text = Resource1.Write;
+            button3.Text = Resource1.Delete;
 
             listBox1.DataSource = users;
             listBox1.ValueMember = "ID";
@@ -50,6 +51,12 @@ namespace UserMaintenance
                 }
                 sw.Close();
             }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int ID = listBox1.SelectedIndex;
+            users.RemoveAt(ID);
         }
     }
 }
