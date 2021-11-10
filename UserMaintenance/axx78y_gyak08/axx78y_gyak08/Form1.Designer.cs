bd7db25ad74f969c5cf8a5e301fe972a29db9092
corvinus-axx78y <levente.virag@stud.uni-corvinus.hu>
2021-11-10 18:02:25 +0100

    szinvalaszto, labda

--- UserMaintenance/axx78y_gyak08/axx78y_gyak08/Form1.Designer.cs
+++ UserMaintenance/axx78y_gyak08/axx78y_gyak08/Form1.Designer.cs
@@ -36,11 +36,13 @@ namespace axx78y_gyak08
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.lblNext = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
             this.mainPanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // mainPanel
             // 
+            this.mainPanel.Controls.Add(this.button3);
             this.mainPanel.Controls.Add(this.lblNext);
             this.mainPanel.Controls.Add(this.button2);
             this.mainPanel.Controls.Add(this.button1);
@@ -90,6 +92,16 @@ namespace axx78y_gyak08
             this.lblNext.TabIndex = 2;
             this.lblNext.Text = "Coming Next:";
             // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.button3.Location = new System.Drawing.Point(148, 121);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(94, 33);
+            this.button3.TabIndex = 3;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
@@ -112,6 +124,7 @@ namespace axx78y_gyak08
         private System.Windows.Forms.Label lblNext;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- UserMaintenance/axx78y_gyak08/axx78y_gyak08/Form1.cs
+++ UserMaintenance/axx78y_gyak08/axx78y_gyak08/Form1.cs
@@ -79,5 +79,21 @@ namespace axx78y_gyak08
             _nextToy.Left = lblNext.Left;
             Controls.Add(_nextToy);
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            var colorPicker = new ColorDialog();
+
+            colorPicker.Color = button.BackColor;
+            if (colorPicker.ShowDialog() != DialogResult.OK)
+                return;
+            button.BackColor = colorPicker.Color;
+
+            Factory = new BallFactory
+            {
+                BallColor = button3.BackColor
+            };
+        }
     }
 }
