656d6e5905b2abc8936b452aaacb832f086237df
corvinus-axx78y <levente.virag@stud.uni-corvinus.hu>
2021-11-10 17:43:26 +0100

    auto, gyar absztrakcio kezelese, absztrakcio felhasznalasa

---
+++ UserMaintenance/axx78y_gyak08/axx78y_gyak08/Entities/Car.cs
@@ -0,0 +1,19 @@
+﻿using axx78y_gyak08.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace axx78y_gyak08.Entities
+{
+    public class Car : Toy
+    {
+        protected override void DrawImage(Graphics g)
+        {
+            Image imageFile = Image.FromFile("Images/car.png");
+            g.DrawImage(imageFile, new Rectangle(0, 0, Width, Height));
+        }
+    }
+}
